7e70a89cf13ee065d925a936c8a718bcde435640
Alasdair Gray <alasdairthgray@gmail.com>
2025-12-13 22:44:40 +0000

    added shit i forgot

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,21 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
+import remarkBreaks from 'remark-breaks';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+	vite: {
+		plugins: [tailwindcss()]
+	},
 
-  integrations: [mdx()]
+	integrations: [
+		mdx({
+			remarkPlugins: [remarkBreaks],
+			rehypePlugins: [],
+			extendMarkdownConfig: {
+				gfm: true
+			}
+		})
+	]
 });
